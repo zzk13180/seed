b124cb3a734a635ca47d924d942ed385df9ce226
zhangzhankui <zzk13180@gmail.com>
2022-11-06 16:48:00 +0800

    feat: add vue composables lib build sample

---
+++ packages/vue-composables/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import { viteConfig } from '@seed/configs'
+
+export default defineConfig(env => {
+  const plugins = [VueJsx(), Vue()]
+  return {
+    ...viteConfig(env),
+    publicDir: 'public',
+    envDir: 'env',
+    plugins,
+    build: {
+      outDir: './dist/',
+      emptyOutDir: true,
+      sourcemap: false,
+      lib: {
+        entry: 'src/index.ts',
+        name: 'vue-composables',
+      },
+      rollupOptions: {
+        external: ['vue'],
+      },
+    },
+  }
+})
